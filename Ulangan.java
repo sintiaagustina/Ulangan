123ca259772b02c8ade2787743539ba31f8afb4b
sintiaagustina <59639650+sintiaagustina@users.noreply.github.com>
2020-09-08 09:43:39 +0700

    Add files via upload

---
+++ Ulangan.java
@@ -0,0 +1,28 @@
+
+package ulangan;
+
+
+public class Ulangan {
+    public static void main(String[] args) {
+        String str = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        char ch1 = str . charAt(18);
+        char ch2 = str . charAt(8);
+        char ch3 = str . charAt(13);
+        char ch4 = str . charAt(19);
+        char ch5 = str . charAt(8);
+        char ch6 = str . charAt(0);
+        char ch7 = str . charAt(0);
+        char ch8 = str . charAt(6);
+        char ch9 = str . charAt(20);
+        char ch10 = str . charAt(18);
+        char ch11 = str . charAt(19);
+        char ch12 = str . charAt(8);
+        char ch13 = str . charAt(13);
+        char ch14 = str . charAt(0);
+        
+        System.out.println (" "+ch1+ch2+ch3+ch4+ch5+ch6);
+        System.out.println (" "+ch7+ch8+ch9+ch10+ch11+ch12+ch13+ch14);
+        
+    }
+    
+}
